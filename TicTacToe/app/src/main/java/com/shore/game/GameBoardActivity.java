dea05b59dff138a78d6a297ea26d91c373fb455c
mostafazakaria <mostafa.a.zakaria>
2016-02-29 00:21:38 +0200

    Created an empty android studio project

---
+++ TicTacToe/app/src/main/java/com/shore/game/GameBoardActivity.java
@@ -0,0 +1,13 @@
+package com.shore.game;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
